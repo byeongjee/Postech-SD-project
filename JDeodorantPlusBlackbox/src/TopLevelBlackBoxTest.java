4fc6be643445f61beaea8a859acf5e138f1d511f
Oh sukyung <ohsk23@postech.ac.kr>
2018-11-06 19:53:55 +0900

    Add test codes for menu tabs with Junyoung Kim

--- JDeodorantPlusBlackbox/src/TopLevelBlackBoxTest.java
+++ JDeodorantPlusBlackbox/src/TopLevelBlackBoxTest.java
@@ -28,15 +28,24 @@ public class TopLevelBlackBoxTest {
 	}
 
 	@Test
-	public void testOpenJDeodorantPlusTab() {
-		openSpeculativeGeneralityTab();
+	public void testOpenSpeculativeGeneralityTab() {
+		bot.menu("Bad Smells").menu("Speculative Generality").click();
 		bot.viewByTitle("Speculative Generality");
 	}
 	
-	private void openSpeculativeGeneralityTab() {
-		bot.menu("Bad Smells").menu("Speculative Generality").click();
+	@Test
+	public void testOpenMessageChainTab() {
+		bot.menu("Bad Smells").menu("Message Chain").click();
+		bot.viewByTitle("Message Chain");
 	}
 	
+	@Test
+	public void testOpenLongParameterListTab() {
+		bot.menu("Bad Smells").menu("Long Parameter List").click();
+		bot.viewByTitle("Long Parameter List");
+	}
+	
+
 	private void openPackageExplorer() {
 		bot.menu("Window").menu("Show View").menu("Other...").click();
 		SWTBotShell dialog = bot.shell("Show View");
