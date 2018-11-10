b3ac6a94ad0590f0e53e099d48d1521031496bf8
Junyoung Kim <junyoung2@postech.ac.kr>
2018-11-10 21:32:20 +0900

    Add JUnit test template for LongMethod.java with Sukyung Oh

---
+++ JDeodorant/src/junittest/LongMethodJUnitTest.java
@@ -0,0 +1,14 @@
+package junittest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class LongMethodJUnitTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
